9b8a74af23c332fb0819e232cd46bdab0d8d7abe
MagaliDrumare <mag250475@gmail.com>
2022-05-02 11:40:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,7 +155,7 @@ def main():
 
 if selected == "Demo (5 phrases max)":
 
-    API_KEY = st.secrets["API_KEY"]
+    API_KEY = st.secrets["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"]
 
     API_URL = (
         "https://api-inference.huggingface.co/models/valhalla/distilbart-mnli-12-3"
